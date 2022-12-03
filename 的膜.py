1fcb479b2ce43907f3d306f235ab1bb2e74ce5a8
zpstudent <zpstudent@yeah.net>
2022-12-03 19:14:47 +0800

    111

--- 的膜.py
+++ 的膜.py
@@ -3,13 +3,15 @@
 # @Author:south wind
 # @Date:2022-12-01
 # @IDE:PyCharm
-def nearestValidPoint(x: int, y: int, points):
-    output = -1
-    res = -1
-    for i, (a, b) in enumerate(points):
-        if a == x or b == y:
-            if output > (abs(a - x) + abs(b - y)):
-                res = i
-    return res
-x=nearestValidPoint(3,4,[[1,2],[3,1],[2,4],[2,3],[4,4]])
-print(x)
+def minOperations(boxes: str):
+    initial_state = []
+    n = len(boxes)
+    for i in range(n):
+        if boxes[i] == '1':
+            num_i = [0] * n
+            for j in range(n):
+                num_i[j] = abs(j - i)
+            initial_state.append(num_i)
+    print(initial_state)
+    return [sum(initial_state[i][:]) for i in range(n)]
+print(minOperations('110'))
